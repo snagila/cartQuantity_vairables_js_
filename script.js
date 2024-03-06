f56d3eb1e18d452fd485ccfa5283efb2ab4f2445
Sachin Nagila <sachinnagila@192-168-1-106.tpgi.com.au>
2024-03-06 12:37:48 +1100

    cart_quantity_onlywithjavavariables_inline_html

---
+++ script.js
@@ -0,0 +1,45 @@
+// //single line comment.This only comments the single  line
+
+// /* multiple line comment
+// until this mark in the front is closec
+// this is used to comment multiple lines at once*/
+
+// // VARIABLE {ONLY(CHARACTERR NUMBER AND $)}
+// // var name="prem"
+// // let name="prem"
+// // const name="acharya"
+
+// // var firstName = "Prem Acharya";
+// // let address = "sydney";
+// // const dob = "2020-20-02";
+// // console.log(firstName, address, dob);
+
+// // Arithmetic
+// // +	Addition
+// // -	Subtraction
+// // *	Multiplication
+// // **	Exponentiation (ES2016)
+// // /	Division
+// // %	Modulus (Division Remainder)
+// // ++	Increment
+// // --	Decrement
+
+// // let a = 5;
+// // const b = 3;
+
+// // // console.log(++a); if ++/-- is before variable then adds in the same line but if the ++ is after the variable then it is added on the next
+// // console.log(a);
+
+// // LOGICAL OPERATORS
+// // &&
+// // ||
+// // !
+// // Ternary
+
+// // DATA TYPES
+// // PRIMITIVES
+// const a = 1123;
+// console.log(typeof null);
+
+// REFERENCE TYPE
+// OBJECT== {KEY:"VALUE"}
